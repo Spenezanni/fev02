19eceaf947f836f084348eae332d3f6266900848
Luiz Gustavo <gustavospenezanni@gmail.com>
2021-02-04 16:02:09 -0300

    implementation two classes

---
+++ src/main/java/br/com/fev02/model/Cliente.java
@@ -0,0 +1,31 @@
+package br.com.fev02.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "CLIENTE")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "NOME")
+	private String nome;
+	
+	@Column(name = "CPF")
+	private String cpf;
+	
+	@Column(name = "DESCRICAO")
+	private String descricao;
+	
+	@OneToOne
+	private Endereco endereco;
+
+}
